8af76ab3f80bf2a9ed766aea9ad4d9b01e36673c
Kittikan Kongoat <97511935+KittikanK@users.noreply.github.com>
2022-12-14 21:17:57 +0700

    V.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,45 @@
 <template>
   <v-card fluid class="fill-height bg-blue-lighten-5">
-    <h1 class="pa-10">Home</h1>
+    <div id="nevbar" class="roundconner">
+        <h1> MEETANG </h1>
+        <div id="buttons">
+            <v-btn col>
+                Home
+            </v-btn>
+            <v-btn>
+                About
+            </v-btn>
+            <v-btn color="#77ADFF" style="color:#FFFFFF;">
+                Log in
+            </v-btn>
+            <v-btn color="#274DD5" style="color:#FFFFFF;">
+                Sign up
+            </v-btn>
+        </div>
+    </div>
     <router-link to="/app">
-      <v-btn color="primary">Go to App</v-btn>
+      <v-btn color="primary" elevation="3" >
+        เริ่มต้นใช้งาน
+      </v-btn>
     </router-link>
   </v-card>
-
-
 </template>
 
-<script>
-export default {
-name: "HomeView",
-components: {
-  // SideMenu,
-},
-};
-
-</script>
+<style> 
+  #nevbar {
+    position: fixed;
+    padding: 6px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80vw;
+    margin: 50px auto;
+    left: 0;
+    right: 0;
+  }
+  
+.roundconner {
+    border: 3px solid #4198FD;
+    border-radius: 10px;
+}
+</style>
