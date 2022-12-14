a7518b4b267897e977e755cb80d93ec1bc59dfd7
owensocool <63050121@kmitl.ac.th>
2022-12-14 17:34:42 +0700

    Update3

--- src/views/app/detailshop.vue
+++ src/views/app/detailshop.vue
@@ -1,3 +1,122 @@
 <template>
-    <h1>มาดามตามสั่ง - วัดปากลมบน</h1>
+    <h1 class="d-flex justify-start mb-0">มาดามตามสั่ง - วัดปากลมบน</h1>
+
+
+    <v-card
+      class="d-flex justify-start mb-4"
+      flat
+      tile
+    >
+      <v-card
+        class="pa-2"
+        outlined
+        tile
+      >
+      4.9 • 0.3 km เวลาเปิดให้บริการ 14.00 น. - 18.00 น. 
+      </v-card>
+
+    </v-card>
+    <v-tabs
+    fixed-tabs
+    bg-color="indigo-darken-2"
+  >
+    <v-tab>
+      โปรโมชั่นพิเศษ
+    </v-tab>
+    <v-tab>
+      เมนูแนะนำ
+    </v-tab>
+    <v-tab>
+      เครื่องดื่ม
+    </v-tab>
+    <v-tab>
+      ของหวาน
+    </v-tab>
+  </v-tabs>
+
+  <h1 class="d-flex justify-start mb-4">โปรโมชั่นพิเศษ</h1>
+    <v-card
+    class="pa-2"
+    max-width="344"
+    variant="outlined"
+  >
+    <v-card-item>
+      <div>
+        <div class="text-h6 mb-16">
+            "ข้าวมันไก่+โค๊ก"
+        </div>
+        <div class="text-caption "> 60 บาท</div>
+      </div>
+    </v-card-item>
+    <v-card-actions>
+      <v-btn variant="tonal">
+        <v-icon icon="mdi-plus" size="large"></v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  <h1 class="d-flex justify-start mb-4">เมนูแนะนำ</h1>
+    <v-card
+    class="pa-2"
+    max-width="344"
+    variant="outlined"
+  >
+    <v-card-item>
+      <div>
+        <div class="text-h6 mb-16">
+            "ข้าวมันไก่"
+        </div>
+        <div class="text-caption "> 50 บาท</div>
+      </div>
+    </v-card-item>
+    <v-card-actions>
+      <v-btn variant="tonal">
+        <v-icon icon="mdi-plus" size="large"></v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  <h1 class="d-flex justify-start mb-4">เครื่องดื่ม</h1>
+    <v-card
+    class="pa-2"
+    max-width="344"
+    variant="outlined"
+  >
+    <v-card-item>
+      <div>
+        <div class="text-h6 mb-16">
+            "เลือดมมาดาม"
+        </div>
+        <div class="text-caption "> 20 บาท</div>
+      </div>
+    </v-card-item>
+    <v-card-actions>
+      <v-btn variant="tonal">
+        <v-icon icon="mdi-plus" size="large"></v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  <h1 class="d-flex justify-start mb-4">ของหวาน</h1>
+    <v-card
+    class="pa-2"
+    max-width="344"
+    variant="outlined"
+  >
+    <v-card-item>
+      <div>
+        <div class="text-h6 mb-16">
+            "หังใจคราเค่น"
+        </div>
+        <div class="text-caption "> 20 บาท</div>
+      </div>
+    </v-card-item>
+    <v-card-actions>
+      <v-btn variant="tonal">
+        <v-icon icon="mdi-plus" size="large"></v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+  
 </template>
