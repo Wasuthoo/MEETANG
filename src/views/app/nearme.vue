5f636e396f57a6efbb04ba2c2ffb9971787329d7
Mozzy <tiwter.tj99@gmail.com>
2022-12-15 01:23:20 +0700

    Update nearme.vue

--- src/views/app/nearme.vue
+++ src/views/app/nearme.vue
@@ -1,26 +1,43 @@
 <template>
     <h1 class="pa-5">Food near me </h1>
     <hr/>
-    <h2 class="pa-5">Meetang
+    
+    <v-row>
+
+        <v-col>
+
+            <h2 class="pa-5">
+
+                Meetang
+
+            </h2> 
+
+        </v-col>
+        
+        <v-col>
 
         <v-form>
 
-        <v-container>           
+            <v-container>           
 
-        <v-text-field
-            solo
-            label="กรุณาระบุตำแหน่งปัจจุบันของมึง"
-            append-icon="mdi-map-marker"
-        ></v-text-field>
+                <v-text-field 
+                    solo
+                    label="กรุณาระบุตำแหน่งปัจจุบันของคุณ"
+                    append-icon="mdi-map-marker"
+            >
+            </v-text-field>
 
-        </v-container>
+            </v-container>
 
         </v-form> 
+     
+        </v-col>
 
-    </h2>
+    </v-row>  
 
   <div>
 
+    <!-- แถว 1 -->
     <v-row>
 
         <v-col>
@@ -126,7 +143,229 @@
 
     </v-row>
 
+    <!-- แถว 2 -->
+    <v-row>
+
+    <v-col>
+
+    <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
+
+    <v-img
+        class="white--text align-end"
+        height="200px"
+        src="/shop/Shop1BG.png"
+    >
+    </v-img>
+
+    <v-card-title>
+        มาดามตามสั่ง - วัดปากลมบน
+    </v-card-title>
+
+    <v-card-subtitle>
+        \n
+    </v-card-subtitle>
+
+    <v-card-subtitle class="my-2">
+        <v-icon>
+            mdi-star
+        </v-icon>
+        4.9
+        <v-icon>
+            mdi-walk
+        </v-icon>
+        0.3 km
+    </v-card-subtitle>
+
+    </v-card>
+
+    </v-col>
+
+    <v-col>
+        <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
+
+    <v-img
+        class="white--text align-end"
+        height="200px"
+        src="/shop/Shop2BG.png"
+    >
+    </v-img>
+
+    <v-card-title>
+        มาดามตามสั่ง - วัดปากลมบน
+    </v-card-title>
+
+    <v-card-subtitle>
+        \n
+    </v-card-subtitle>
+
+    <v-card-subtitle class="my-2">
+        <v-icon>
+            mdi-star
+        </v-icon>
+        4.9
+        <v-icon>
+            mdi-walk
+        </v-icon>
+        0.3 km
+    </v-card-subtitle>
+
+    </v-card>
+
+    </v-col>
+
+    <v-col>
+        
+    <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
+
+    <v-img
+        class="white--text align-end"
+        height="200px"
+        src="/shop/Shop3BG.png"
+    >
+    </v-img>
+
+    <v-card-title>
+        มาดามตามสั่ง - วัดปากลมบน
+    </v-card-title>
+
+    <v-card-subtitle>
+        \n
+    </v-card-subtitle>
+
+    <v-card-subtitle class="my-2">
+        <v-icon>
+            mdi-star
+        </v-icon>
+        4.9
+        <v-icon>
+            mdi-walk
+        </v-icon>
+        0.3 km
+    </v-card-subtitle>
+
+    </v-card>
+
+    </v-col>
+
+    </v-row>
+
+<!-- แถวที่ 3 -->
+    <v-row>
+
+<v-col>
+
+<v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
+
+<v-img
+    class="white--text align-end"
+    height="200px"
+    src="/shop/Shop1BG.png"
+>
+</v-img>
+
+<v-card-title>
+    มาดามตามสั่ง - วัดปากลมบน
+</v-card-title>
+
+<v-card-subtitle>
+    \n
+</v-card-subtitle>
+
+<v-card-subtitle class="my-2">
+    <v-icon>
+        mdi-star
+    </v-icon>
+    4.9
+    <v-icon>
+        mdi-walk
+    </v-icon>
+    0.3 km
+</v-card-subtitle>
+
+</v-card>
+
+</v-col>
+
+<v-col>
+    <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
+
+<v-img
+    class="white--text align-end"
+    height="200px"
+    src="/shop/Shop2BG.png"
+>
+</v-img>
+
+<v-card-title>
+    มาดามตามสั่ง - วัดปากลมบน
+</v-card-title>
+
+<v-card-subtitle>
+    \n
+</v-card-subtitle>
+
+<v-card-subtitle class="my-2">
+    <v-icon>
+        mdi-star
+    </v-icon>
+    4.9
+    <v-icon>
+        mdi-walk
+    </v-icon>
+    0.3 km
+</v-card-subtitle>
+
+</v-card>
+
+</v-col>
+
+<v-col>
+    
+<v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
+
+<v-img
+    class="white--text align-end"
+    height="200px"
+    src="/shop/Shop3BG.png"
+>
+</v-img>
+
+<v-card-title>
+    มาดามตามสั่ง - วัดปากลมบน
+</v-card-title>
+
+<v-card-subtitle>
+    \n
+</v-card-subtitle>
+
+<v-card-subtitle class="my-2">
+    <v-icon>
+        mdi-star
+    </v-icon>
+    4.9
+    <v-icon>
+        mdi-walk
+    </v-icon>
+    0.3 km
+</v-card-subtitle>
+
+</v-card>
+
+</v-col>
+
+</v-row>
+
     </div>
+    <!-- จบหน้าร้าน -->
+
+    <v-btn
+      class="mx-auto"
+      size="300"
+      variant="outlined"
+      color="blue"
+    >
+      ดูเพิ่มเติม
+    </v-btn>
 
     <v-container class="d-flex justify-end mb-6">
         <v-btn to = 'detailshop'>มาดามตามสั่ง</v-btn>
