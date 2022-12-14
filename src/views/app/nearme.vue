33ce8d58c587fa5f24146bd77d5dfee1dec6ef54
owensocool <63050121@kmitl.ac.th>
2022-12-14 16:11:43 +0700

    Update2

--- src/views/app/nearme.vue
+++ src/views/app/nearme.vue
@@ -1,21 +1,6 @@
 <template>
     <h1 class="pa-5">food near me </h1>
-  <v-row class="pa-5">
-    <v-col class="text-center ma-2 pa-2 height-100">
-      <router-link to="/classroom/teacher">
-        <v-card class="pa-5 bg-blue-lighten-5"> 
-          <h2 class="pa-5">Teacher</h2>
-          <img src="../public/teacher.jpg" alt="Teacher Mo" width="200" height="300">
-        </v-card>
-      </router-link>
-    </v-col>
-    <v-col class="text-center  ma-2 pa-2">
-      <router-link to="/classroom/student">
-        <v-card class="pa-5 bg-red-lighten-5" height="430"> <h2 class="pa-5">Student</h2> 
-          <img src="../public/BEBE.png" alt="Student" width="200" height="230">
-        </v-card>
-        
-      </router-link>
-    </v-col>
-  </v-row>
+    <v-container class="d-flex justify-end mb-6">
+        <v-btn to = 'detailshop'>มาดามตามสั่ง</v-btn>
+    </v-container>
 </template>
