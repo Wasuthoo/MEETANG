730b35cd1d6c70ed2e856aadc7be9e7f68338eec
Kaewphet Nuruang <66639008+nobiofla@users.noreply.github.com>
2022-12-14 21:04:28 +0700

    update cashbook

--- src/views/app/cashbook.vue
+++ src/views/app/cashbook.vue
@@ -1,5 +1,90 @@
 <template>
-    <v-container>
-        <h1>cashbook</h1>
-    </v-container>
+    <div class="heading">
+        <h1 class="title valgin-text-middle heading">CashBook</h1>
+    </div>
+    <div class="content">
+        <div class="overlap-group17">
+            <img class="user-1" src="/icon/user 1.png" alt="user 1"/>
+            <div class="flex-col">
+                <h1 class="text-28 valign-text-middle heading">สรุปรายการความเคลื่อนไหวบัญชีประจำปี</h1>
+                <div class="text-29 valign-text-middle">คุณธนวรรณ ณ กรุงเทพมหานคร</div>
+                <div class="x64050138kmitlacth valigh-text-middle">64050138@kmitl.ac.th</div>
+            </div>
+        </div>
+        <div class="overlap-group-container heading">
+            <div class="text-container">
+                <div class="text-2 valign-text-middle">ยอดเงินเข้ารวม</div>
+                <div class="text-2-1 valign-text-middle">
+                    <span>
+                        <span class="span0 heading">326,000</span>
+                        <span class="span1 heading">&nbsp;</span>
+                        <span class="span2 heading">บาท</span>
+                    </span>
+                </div>
+            </div>
+            <div class="text-container-1">
+                <div class="text-2-2 valign-text-middle">ยอดเงินออกรวม</div>
+                <div class="text-2-3 valign-text-middle">
+                    <span>
+                        <span class="span0 heading">248,000</span>
+                        <span class="span1 heading">&nbsp;</span>
+                        <span class="span2-1 heading">บาท</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="overlap-group12 big-text">
+            <div class="view">
+                <div class="rectangle-13-1"></div>
+                <div class="rectangle-14-1"></div>
+                <div class="view-1">
+                    <div class="overlap-group7">
+                        <div class="rectangle-13-2"></div>
+                        <div class="rectangle-14-2"></div>
+                    </div>
+                    <div class="view-2">
+                        <div class="rectangle-13-3"></div>
+                        <div class="rectangle-14-3"></div>
+                        <div class="view-3">
+                            <div class="overlap-group-1">
+                                <div class="rectangle-13-4"></div>
+                                <div class="rectangle-14-4"></div>
+                            </div>
+                        </div>
+                        <div class="view-4">
+                            <div class="overlap-group">
+                                <div class="rectangle-13"></div>
+                                <div class="rectangle-14"></div>
+                            </div>
+                            <div class="view-5">
+                                <div class="overlap-group5">
+                                    <div class="rectangle-13-5"></div>
+                                    <div class="regtangle-14-5"></div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style>
+    .heading {
+        display: flex;
+        flex-direction: column;
+        height: 99px;
+        width: 1337px;
+    }
+    
+    .titile {
+        color: var(--black);
+        font-weight: 800;
+        height: 99px;
+        line-height: normal;
+        text-align: center;
+        width: 234px;
+    }
+</style>
