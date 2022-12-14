dcb2b61b326fee50692f72a6dc71fc51e2908c10
Wasu <67475566+Wasuthoo@users.noreply.github.com>
2022-12-14 22:38:52 +0700

    Update dashboard.vue

--- src/views/app/dashboard.vue
+++ src/views/app/dashboard.vue
@@ -1,60 +1,99 @@
 <template>
-   <h1>Dashboard</h1>
-      <div class="summery">
-         <div class="flex-col">
-            <div class="flex-col-1">
-               <div class="summary-1 valign-text-middle big-text">Summary</div>
-               <img class="Oo-1" src="/icon/o.png" alt="O" />
-            </div>
-         </div>
-      </div>
+   <v-container fluid class="bg-pink-lighten-4">
+      <h1>Dashboard</h1>
+      <br />
+      <v-row>
+         <v-col>
+            <v-card class="ma-2 pa-5">
+               <h1>Summary1</h1>
+               <img class="Oo-1" src="/icon/o.png" alt="O" height="100" />
+            </v-card>
+         </v-col>
+
+         <v-col>
+            <v-card class="ma-2 pa-5">
+               <h1>Summary2</h1>
+               <img class="Oo-1" src="/icon/o.png" alt="O" height="100" />
+            </v-card>
+         </v-col>
+      </v-row>
+
+      <v-row>
+
+         <v-col>
+            <v-card class="ma-2 pa-5">
+               <h1>Summary3</h1>
+               <img class="Oo-1" src="/icon/o.png" alt="O" height="100" />
+            </v-card>
+         </v-col>
+
+         <v-col>
+            <v-card class="ma-2 pa-5">
+               <h1>Summary4</h1>
+               <img class="Oo-1" src="/icon/o.png" alt="O" height="100" />
+            </v-card>
+         </v-col>
+
+      </v-row>
+      <v-card class="mt-5 mx-2 pa-5">
+               <h1>Summary5</h1>
+               <img class="Oo-1" src="/icon/o.png" alt="O" height="100" />
+            </v-card>
+
+   </v-container>
 </template>
 
+
+
+
+
+<!-- 
 <style>
-.summary-1
-{
-   color: var(--black) ;
+.summary-1 {
+   rowor: var(--black);
    font-weight: 600;
    height: 30px;
    line-height: normal;
 }
-.flex-col {
-align-items: center;
-background-color: var(--white) ;
-border-radius: 20px;
-box-shadow: 0px 4px 4px #00000040;
-display: flex;
-flex-direction: column;
-gap: 21px;
-min-height: 257px;
-padding: 13px 0;
-width: 257px;
-}
-.flex-col-1{
-   align-items:flex-start;
-   display: flex;
-   flex-direction: column;
-   gap:8px;
-   min-height:39px;
-   width:594px;
-}
+
 .flex-row {
-   align-items:flex-start;
+   align-items: center;
+   background-rowor: var(--white);
+   border-radius: 20px;
+   box-shadow: 0px 4px 4px #00000040;
    display: flex;
-   height:171px;
+   flex-direction: rowumn;
+   gap: 21px;
+   min-height: 257px;
+   padding: 13px 0;
+   width: 257px;
+}
+
+.flex-row-1 {
+   align-items: flex-start;
+   display: flex;
+   flex-direction: rowumn;
+   gap: 8px;
+   min-height: 39px;
+   width: 594px;
+}
+
+.flex-row {
+   align-items: flex-start;
+   display: flex;
+   height: 171px;
    margin-right: 6px;
    min-width: 550px;
 
 }
-.Oo-1{
 
-}
-.rectangle-152 
-{
-   background-color: var(--white) ;
+.Oo-1 {}
+
+.rectangle-152 {
+   background-rowor: var(--white);
    border-radius: 20px;
    box-shadow: 0px 4px 4px #000000;
    height: 257px;
    width: 594px;
 }
-</style>
+</style> -->
