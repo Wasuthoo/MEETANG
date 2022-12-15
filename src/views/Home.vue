d4c86f6cbe6558aad2519faa3c30b243838c7332
Wasu <67475566+Wasuthoo@users.noreply.github.com>
2022-12-15 16:45:13 +0700

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,36 @@
 <template>
   <v-card fluid class="fill-height bg-blue-lighten-5">
-    <div id="nevbar" class="roundconner">
-        <h1> MEETANG </h1>
-        <div id="buttons">
-            <v-btn col>
-                Home
-            </v-btn>
-            <v-btn>
-                About
-            </v-btn>
-            <v-btn color="#77ADFF" style="color:#FFFFFF;" to="/login">
-                Log in
-            </v-btn>
-            <v-btn color="#274DD5" style="color:#FFFFFF;" to="/login">
-                Sign up
-            </v-btn>
+    
+    <!-- NavBar -->
+    <v-card class="rounded-xl pa-5 ma-10 d-flex justify-space-between" 
+      style="border: 3px solid #4198FD; border-radius: 10px;"> 
+        <div class="d-flex">
+          <v-img src="../../public/m.png" style="height : 50px; width:50px;"/>
+          <div>
+            <h3 class="pa-3 mx-4">MEETANG</h3>
+            <a></a>
+          </div>
         </div>
-    </div>
-    <router-link to="/app">
-      <v-btn color="primary" elevation="3" >
-        เริ่มต้นใช้งาน
-      </v-btn>
-    </router-link>
+      <v-spacer></v-spacer>
+      <div id="buttons" class="py-2">
+        <v-btn col class="mx-2" to="/">
+          Home
+        </v-btn>
+        <v-btn class="mx-2" to="/about">
+          About
+        </v-btn>
+        <v-btn class="mx-2" color="#77ADFF" style="color:#FFFFFF;" to="/login">
+          Log in
+        </v-btn>
+        <v-btn class="mx-2" color="#274DD5" style="color:#FFFFFF;" to="/login">
+          Sign up
+        </v-btn>
+      </div>
+    </v-card>
+    <!-- end of NavBar -->
+
+    
+
+
   </v-card>
 </template>
-
-<style> 
-  #nevbar {
-    position: fixed;
-    padding: 6px 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 80vw;
-    margin: 50px auto;
-    left: 0;
-    right: 0;
-  }
-  
-.roundconner {
-    border: 3px solid #4198FD;
-    border-radius: 10px;
-}
-</style>
