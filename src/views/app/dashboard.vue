6e877d8efbe3b9253afac3b5a32c36465d5231a0
Tanawat <63050138@kmitl.ac.th>
2022-12-15 17:33:03 +0700

    update Dashborad
    บัครูปไม่ขึ้น

--- src/views/app/dashboard.vue
+++ src/views/app/dashboard.vue
@@ -1,63 +1,541 @@
 <template>
-   <v-container fluid class="bg-pink-lighten-4">
-      <h1>Dashboard</h1>
-      <br />
-      <v-row>
-         <v-col>
-            <v-card class="ma-5 pa-5">
-               <h1>Summary</h1>
-               <div class="d-flex" > 
-                  <img class="Oo-1" src="/icon/o.png" alt="O" height="200" /> 
-                     <v-card class="ml-lg-auto bg-green lighten-3" height="60" to width="120px">
-                        <div class="ml-2" >
-                           <h5> Income </h5>
-                              <div class="ml-lg-auto"> 
-                                 <h3> 15,000 baht </h3> 
-                              </div>
-                        </div>
-                     </v-card>
-                     <br/>
-                  <v-card class="ml-lg-auto bg-red lighten-3" height="60" to width="120px">
-                        <div class="ml-2" >
-                           <h5> Expense </h5>
-                              <div class="ml-lg-auto"> 
-                                 <h3> 3,000 baht </h3> 
-                              </div>
-                        </div>
-                     </v-card>
+  <h1>Dashboard</h1>
+  <div class="content">
+    <div class="overlap-group-container">
+      <div class="overlap-group-1">
+        <div class="rectangle-15">
+          <div class="text-1-1 valign-text-middle poppins-semi-bold-shark-16px">
+            1,000
+          </div>
+          <div class="date-1 valign-text-middle normal-text">12/04/22</div>
+          <div class="text-2 valign-text-middle poppins-semi-bold-shark-16px">
+            1,000
+          </div>
+          <div class="date-2 valign-text-middle normal-text">12/04/22</div>
+          <div class="number valign-text-middle poppins-semi-bold-shark-16px">
+            500
+          </div>
+          <div class="date-3 valign-text-middle normal-text">11/04/22</div>
+          <div class="number-1 valign-text-middle poppins-semi-bold-shark-16px">
+            5000
+          </div>
+          <div class="date-4 valign-text-middle normal-text">09/04/22</div>
+          <div class="ellipse-19"></div>
+          <div class="ellipse-20"></div>
+          <div class="ellipse-21"></div>
+          <div class="ellipse-22"></div>
+          <img class="line-1" src="/imageDash/line-3.png" alt="Line 3" />
+          <div class="expense valign-text-middle big-text">Expense</div>
+          <img class="line-12" src="/imageDash/line-12.png" alt="Line 12" />
+          <img class="line-13" src="/imageDash/line-13.png" alt="Line 13" />
+          <img class="line-14" src="/imageDash/line-14.png" alt="Line 14" />
+          <img class="line-15" src="/imageDash/line-15.png" alt="Line 15" />
+          <div class="text-3 valign-text-middle big-text">ค่าเช่าบ้าน</div>
+          <div class="text-4 valign-text-middle big-text">ค่าโทรศัพท์</div>
+          <div class="text-5 valign-text-middle big-text">ค่าอาหาร</div>
+          <div class="text-6 valign-text-middle big-text">ค่าเสื้อผ้า</div>
+          <div class="wallet valign-text-middle normal-text">Wallet</div>
+          <div class="wallet-1 valign-text-middle normal-text">Wallet</div>
+          <div class="wallet-2 valign-text-middle normal-text">Wallet</div>
+          <div class="wallet-3 valign-text-middle normal-text">Wallet</div>
+        </div>
+        <div class="overlap-group1">
+          <div class="rectangle-15"></div>
+          <img class="line-3-1" src="/imageDash/line-3.png" alt="Line 3" />
+          <div class="income valign-text-middle big-text">Income</div>
+          <div class="group-258">
+            <div class="flex-roe">
+              <div class="ellipse-container">
+                <div class="ellipse"></div>
+                <div class="ellipse"></div>
+                <div class="ellipse"></div>
+                <div class="ellipse"></div>
+              </div>
+              <div class="flex-col">
+                <div class="text-7 valign-text-middle big-text">ฝากเงิน</div>
+                <div class="wallet-4 valign-text-middle normal-text">
+                  Wallet
+                </div>
+                <div class="text valign-text-middle big-text">ฝากเงิน</div>
+                <div class="wallet-5 valign-text-middle normal-text">
+                  Wallet
+                </div>
+                <div class="text valign-text-middle big-text">ฝากเงิน</div>
+                <div class="wallet-6 valign-text-middle normal-text">
+                  Wallet
+                </div>
+                <div class="text valign-text-middle big-text">หนูนาพาโชค</div>
+                <div class="wallet-7 valign-text-middle normal-text">
+                  Wallet
+                </div>
+              </div>
+            </div>
+            <div class="flex-col-1">
+              <div
+                class="text-11 valign-text-middle poppins-semi-bold-shark-16px"
+              >
+                5,000
+              </div>
+              <div class="date-5 valign-text-middle normal-text">11/04/22</div>
+              <div
+                class="text-12 valign-text-middle poppins-semi-bold-shark-16px"
+              >
+                5,000
+              </div>
+              <div class="date valign-text-middle normal-text">11/03/22</div>
+              <div
+                class="text-1 valign-text-middle poppins-semi-bold-shark-16px"
+              >
+                2,000
+              </div>
+              <div class="date valign-text-middle normal-text">11/02/22</div>
+              <div
+                class="text-1 valign-text-middle poppins-semi-bold-shark-16px"
+              >
+                3,000
+              </div>
+              <div class="date-6 valign-text-middle normal-text">16/01/22</div>
+            </div>
+            <div class="line-container">
+              <img class="line-1" src="/imageDash/line-12-1.png" alt="Line 12" />
+              <img class="line-1" src="/imageDash/line-12-1.png" alt="Line 13" />
+              <img class="line-1" src="/imageDash/line-12-1.png" alt="Line 14" />
+              <img class="line-1" src="/imageDash/line-12-1.png" alt="Line 15" />
+            </div>
+          </div>
+        </div>
+        <div class="overlap-group2">
+          <div class="rectangle-151"></div>
+          <div class="group-257 big-text">
+            <div class="today valign-text-middle">Today</div>
+            <div class="address valign-text-middle">5 December 2022</div>
+          </div>
+          <div class="income-1">
+            <img class="image-6" src="/imageDash/image-6.png" alt="image 6" />
+            <img class="image-item" src="/imageDash/image-6.png" alt="image 7" />
+            <img class="image-item" src="/imageDash/image-6.png" alt="image 8" />
+            <img class="image-item" src="/imageDash/image-2.png" alt="image 2" />
+            <div class="overlap-group-2">
+              <div class="income-2 valign-text-middle">Income</div>
+              <div class="rectangle-168"></div>
+              <div
+                class="
+                  address-1
+                  valign-text-middle
+                  poppins-bold-caribbean-green-20px
+                "
+              >
+                <span
+                  ><span class="poppins-bold-caribbean-green-20px">0</span>
+                  <span class="span1 normal-text">baht</span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="expense-1">
+            <img class="image-5" src="/imageDash/image-5.png" alt="image 5" />
+            <img class="image-4" src="/imageDash/image-4.png" alt="image 4" />
+            <img class="diet-1" src="/imageDash/diet-1.png" alt="diet 1" />
+            <div class="flex-row-1">
+              <div class="overlap-group1-1 poppins-bold-jasper-20px">
+                <div class="expense-2 valign-text-middle">Expense</div>
+                <div class="rectangle-169"></div>
+                <div class="address-2 valign-text-middle">
+                  <span
+                    ><span class="poppins-bold-jasper-20px">0</span
+                    ><span class="span1 normal-text">baht</span>
+                  </span>
+                </div>
+              </div>
+              <img class="dress-1" src="/imageDash/dress-1.png" alt="dress 1" />
+            </div>
+          </div>
+          <img class="line-5" src="/imageDash/line-3.png" alt="Line 5" />
+          <img class="image-34" src="/imageDash/image-34.png" alt="image 34" />
+          <img class="image-33" src="/imageDash/image-33.png" alt="image 33" />
+        </div>
+        <div class="overlap-group3">
+          <div class="accounts valign-text-middle big-text">Accounts</div>
+          <img class="line-3-2" src="/imageDash/line-3.png" alt="Line 3" />
+          <div class="wallet-8">
+            <div class="flex-col-2">
+              <div class="wallet-9 valign-text-middle big-text">Wallet</div>
+              <div class="last-used-12032022 valign-text-middle normal-text">
+                Last used 12/03/2022
+              </div>
+            </div>
+            <div
+              class="
+                x15000-baht
+                valign-text-middle
+                poppins-bold-caribbean-green-20px
+              "
+            >
+              <span
+                ><span class="poppins-bold-caribbean-green-20px">15,000</span>
+                <span class="span1 normal-text">baht</span>
+              </span>
+            </div>
+          </div>
+          <img class="line-4" src="/imageDash/line-4.png" alt="Line 4" />
+          <div class="savings-goal">
+            <div class="flex-col-3">
+              <div class="savings-goal-1 valign-text-middle big-text">
+                Savings Goal
+              </div>
+              <p
+                class="
+                  i-phone-16-max-ranger-gucci-belt
+                  valign-text-middle
+                  normal-text
+                "
+              >
+                iPhone 16 Max Ranger <br />
+                Gucci Belt
+              </p>
+            </div>
+            <div
+              class="
+                address-3
+                valign-text-middle
+                poppins-bold-caribbean-green-20px
+              "
+            >
+              <span
+                ><span class="poppins-bold-caribbean-green-20px">500</span>
+                <span class="span1 normal-text">baht</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="flex-col-4">
+          <div class="flex-col-5">
+            <div class="summary valign-text-middle big-text">Summary</div>
+            <img class="line-3-3" src="/imageDash/line-3.png" alt="Line 3" />
+          </div>
+          <div class="flex-row-2">
+            <div class="overlap-group1-2 big-text">
+              <img class="ellipse-1" src="/imageDash/ellipse-18.png" alt="Ellipse 18" />
+              <img class="ellipse-1" src="/imageDash/ellipse-17.png" alt="Ellipse 17" />
+              <img class="line-11" src="/imageDash/line-11.png" alt="Line 11" />
+              <img class="line-11-1" src="/imageDash/line-11-1.png" alt="Line 11" />
+              <div class="number-2 valign-text-middle">79</div>
+              <div class="number-3 valign-text-middle">21</div>
+            </div>
+            <div class="flex-col-6">
+              <div class="overlap-group">
+                <div class="income-3 valign-text-middle">Income</div>
+                <div class="rectangle-168-1"></div>
+                <div
+                  class="
+                    x15000-baht-1
+                    valign-text-middle
+                    poppins-bold-caribbean-green-20px
+                  "
+                >
+                  <span
+                    ><span class="poppins-bold-caribbean-green-20px">0</span>
+                    <span class="span1 normal-text">baht</span>
+                  </span>
+                </div>
+              </div>
+              <div class="group-container">
+                <div class="group-255">
+                  <div class="group-256">
+                    <div class="overlap-group-3">
+                      <div class="address-4 valign-text-middle des-icon">
+                        365 days
+                      </div>
+                    </div>
                   </div>
-            </v-card>
-         </v-col>
-
-         <v-col>
-            <v-card class="ma-2 pa-5">
-               <h1>Income</h1>
-
-            </v-card>
-         </v-col>
-      </v-row>
-
-      <v-row>
-
-         <v-col>
-            <v-card class="ma-2 pa-5">
-               <h1>Accounts</h1>
-
-            </v-card>
-         </v-col>
-
-         <v-col>
-            <v-card class="ma-2 pa-5">
-               <h1>Expense</h1>
-
-            </v-card>
-         </v-col>
-
-      </v-row>
-      <v-card class="mt-5 mx-2 pa-5">
-               <h1>Today</h1>
-
-            </v-card>
-
-   </v-container>
+                  <div class="group-253">
+                    <div class="rectangle-166"></div>
+                    <div class="income-4 valign-text-middle des-icon">
+                      Income
+                    </div>
+                  </div>
+                  <div class="group-254">
+                    <div class="rectangle-167"></div>
+                    <div class="expense-3 valign-text-middle des-icon">
+                      Expense
+                    </div>
+                  </div>
+                </div>
+                <div class="overlap-group">
+                  <div class="expense-4 valign-text-middle">Expense</div>
+                  <div class="rectangle-169-1"></div>
+                  <div
+                    class="
+                      x3000-baht-2
+                      valign-text-middle
+                      poppins-bold-jasper-20px
+                    "
+                  >
+                    <span
+                      ><span class="poppins-bold-jasper-20px">3000</span>
+                      <span class="span1 normal-text">baht</span>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+  .content{
+    align-items:flex-start;
+    display:flex;
+    height: 787px;
+    overlap:hidden;
+    width: 1210px;
+  }
+
+  .overlap-group-container{
+    height: 779px;
+    position: absolute;
+    width: 1224px;
+  }
+
+  .overlap-group-1{
+    height: 364px;
+    left: 630px;
+    position: absolute;
+    top: 395px;
+    width: 594px;
+  }
+
+  .rectangle-15{
+    background-color: #ffff;
+    border-radius: 20px;
+    box-shadow: 0px 4px 4px #00000040;
+    height: 364px;
+    left: 0px;
+    position: absolute;
+    top: 0px;
+    width: 580px;
+  }
+
+  .text-1-1{
+    height: 24px;
+    left: 492px;
+    letter-spacing: 0px;
+    line-height: normal;
+    position: absolute;
+    text-align: right;
+    top: 73px;
+  }
+
+  .date-1{
+    color: #666;
+    font-weight: 500;
+    height: 24px;
+    left: 463px;
+    line-height: normal;
+    position: absolute;
+    top: 99px;
+  }
+
+  .text-2{
+    height: 24px;
+    left: 492px;
+    letter-spacing: 0px;
+    line-height: normal;
+    position: absolute;
+    text-align: right;
+    top: 144px;
+  }
+
+  .date-2{
+    color: #666;
+    font-weight: 500;
+    height: 24px;
+    left: 463px;
+    line-height: normal;
+    position: absolute;
+    top: 170px;
+  }
+
+  .number{
+    height: 24px;
+    left: 502px;
+    letter-spacing: 0px;
+    line-height: normal;
+    position: absolute;
+    top: 215px;
+  }
+
+  .date-3{
+    color: #666;
+    font-weight: 500;
+    height: 24px;
+    left: 467px;
+    line-height: normal;
+    position: absolute;
+    top: 242px;
+  }
+
+  .number-1{
+    height: 24px;
+    left: 502px;
+    letter-spacing: 0px;
+    line-height: normal;
+    position: absolute;
+    top: 286px;
+  }
+
+  .date-4{
+    color: #666;
+    font-weight: 500;
+    height: 24px;
+    left: 458px;
+    line-height: normal;
+    position: absolute;
+    top: 312px;
+  }
+
+  .ellipse-19{
+    background-color: rgba(232, 11, 11, 0.867);
+    border-radius: 25px;
+    height: 50px;
+    left: 20px;
+    position: absolute;
+    top: 73px;
+    width: 50px;
+  }
+
+  .ellipse-20{
+    background-color: rgba(11, 232, 15, 0.867);
+    border-radius: 25px;
+    height: 50px;
+    left: 20px;
+    position: absolute;
+    top: 144px;
+    width: 50px;
+  }
+
+  .ellipse-21{
+    background-color: rgba(232, 232, 11, 0.867);
+    border-radius: 25px;
+    height: 50px;
+    left: 20px;
+    position: absolute;
+    top: 215px;
+    width: 50px;
+  }
+
+  .ellipse-22{
+    background-color: rgba(11, 232, 232, 0.867);
+    border-radius: 25px;
+    height: 50px;
+    left: 20px;
+    position: absolute;
+    top: 286px;
+    width: 50px;
+  }
+
+  .line-3{
+    height: 1px;
+    left: 0px;
+    position: absolute;
+    top: 51px;
+    width: 594px;
+  }
+
+  .expense{
+    color: rgb(0, 0, 0);
+    font-weight: 600;
+    height: 30px;
+    left: 20px;
+    line-height: normal;
+    position: absolute;
+    top: 13px;
+  }
+
+  .line-12{
+    height: 50px;
+    left: 546px;
+    position: absolute;
+    top: 73px;
+    width: 1px;
+  }
+
+  .line-13{
+    height: 50px;
+    left: 546px;
+    position: absolute;
+    top: 144px;
+    width: 1px;
+  }
+
+  .line-14{
+    height: 50px;
+    left: 546px;
+    position: absolute;
+    top: 215px;
+    width: 1px;
+  }
+
+  .line-15{
+    height: 50px;
+    left: 546px;
+    position: absolute;
+    top: 286px;
+    width: 1px;
+  }
+
+  .text-3{
+    color: #000;
+    font-weight: 600;
+    height: 30px;
+    left: 92px;
+    line-height: normal;
+    position: absolute;
+    top: 73px;
+  }
+
+  .text-4{
+    color: #000;
+    font-weight: 600;
+    height: 30px;
+    left: 92px;
+    line-height: normal;
+    position: absolute;
+    top: 140px;
+  }
+
+  .text-5{
+    color: #000;
+    font-weight: 600;
+    height: 30px;
+    left: 92px;
+    line-height: normal;
+    position: absolute;
+    top: 212px;
+  }
+
+  .text-6{
+    color: #000;
+    font-weight: 600;
+    height: 30px;
+    left: 92px;
+    line-height: normal;
+    position: absolute;
+    top: 282px;
+  }
+
+  .wallet{
+    color: rgb(0, 0, 0);
+    font-weight: 500;
+    height: 24px;
+    left: 92px;
+    line-height: normal;
+    position: absolute;
+    top: 103px;
+  }
+
+</style>
