20f4c184cecb07255b0d6ad062833010b14f6b00
Wasu <67475566+Wasuthoo@users.noreply.github.com>
2022-12-16 13:33:45 +0700

    Add example to store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,12 +1,72 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+// !!!copy ma yang mai dai kae!!!
+import { defineStore } from "pinia";
+import axios from "axios";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+// ! ----- CONST VARIABLES ----- ! //
+const apiUrl = "http://localhost:3000";
+const subApiFeedback = "/feedback";
 
-  return { count, doubleCount, increment }
-})
+
+// ! ----- MODEL ----- ! //
+export const store_feedback = defineStore({
+  id: "feedback",
+  // Declare variables
+  state: () => ({
+    feedback : [],
+  }),
+
+  // Declare Getter method
+  getters: {
+    getFeedback() {
+      return this.feedback;
+    },
+  },
+
+  // Declare actions method 
+  actions: {
+
+    //get data from database and set it as variable
+    async getQueryFeedback() {
+      try {
+        const res = await axios.get(apiUrl + subApiFeedback);
+        this.feedback = res.data;
+        console.log(
+          "API: [",
+          apiUrl + subApiFeedback,
+          "]: \n",
+          this.feedback
+        );
+      } catch (e) {
+        console.error(e);
+      }
+    },
+
+    //add data to database
+    async addSug(payload) {
+      try {
+        const res = await axios.post(apiUrl + subApiFeedback, payload);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async updateFeedback() {
+      for(let i = 0; i < payload.length; i++){
+        try {
+          this.feedback[i].timestamp = payload[i].status;
+          const res = await axios.put(apiUrl + subApiFeedback + "/" + payload[i].id, payload[i]);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+      
+      try {
+        await axios.patch(apiUrl + subApiFeedback + "/"+ payload.id), {
+          status: close,
+        };
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+  },
+});
--- src/views/app/dashboard.vue
+++ src/views/app/dashboard.vue
@@ -1,259 +1,38 @@
+
 <template>
-  <v-container fluid class="bg-red-lighten-5 pa-12" width="100%">
-    <h1>Dashboard</h1>
-    <hr class="solid">
-    <v-container fluid class="my-6">
+  <v-container fluid class="bg-blue-lighten-5 pa-12" width="100%">
+      <h1>Dashboard</h1>
+      <hr class="solid">
+
+      <!-- Card -->
+      <v-card class="gradient d-flex justify-center pa-6 my-5" >
+          <img class="ma-1" src="/icon/user1.png" alt="user 1" height="120" width="120" />
+          <div class="ma-1 px-5">
+              <h1 class="text-center">สรุปรายการความเคลื่อนไหวบัญชีประจำปี</h1>
+              <h2 class="text-center text-white">คุณธนวรรณ ณ กรุงเทพมหานคร</h2>
+              <h2 class="text-center text-white">64050138@kmitl.ac.th</h2>
+          </div>
+      </v-card>
+      <!-- end of card -->
+
       <v-row>
-        <v-col>
-          <!-- SummaryCard -->
-          <v-card class="pa-2 px-5 rounded-xl ">
-            <h1>Summary</h1>
-            <hr class="solid">
-            <div class="d-flex">
-              <v-img src="/imageDash/Graph.png" height="200" width="200" />
-              <div class="pa-3">
-                <v-card class="bg-green-lighten-5 pa-2 ma-3 rounded-lg" width="320">
-                  <h3>income</h3>
-                  <h1 class="text-end px-2">1,000 Baht</h1>
-                </v-card>
-                <v-card class="bg-red-lighten-5 pa-2 ma-3 rounded-lg" width="320">
-                  <h3>expense</h3>
-                  <h1 class="text-end px-2">3,000 Baht</h1>
-                </v-card>
-              </div>
-            </div>
-          </v-card>
-
-          <!-- AccountCard -->
-          <v-card class="mt-6 pa-2 px-5 rounded-xl ">
-            <h1>Account</h1>
-            <hr class="solid">
-            <div class="d-flex justify-space-between my-1">
-              <div>
-                <h3>Wallet</h3>
-                <p>Last used:12/03/2022</p>
-              </div>
-              <h1 class="text-green">15,000 Baht</h1>
-            </div>
-            <hr class="solid">
-            <h3 class="">Saving goal</h3>
-            <div class="d-flex justify-space-between">
-              <ul class="px-8">
-                <li>Iphone 16 Max Ranger</li>
-                <li>Gucci Belt</li>
-              </ul>
-              <h1 class="text-green my-n3">500 Baht</h1>
-            </div>
-          </v-card>
-
-          <!-- TodayCard -->
-          <v-card class="mt-6 pa-2 px-5 rounded-xl ">
-            <div class="d-flex justify-space-between">
-              <h1>Today</h1>
-              <h2>5 December 2022</h2>
-            </div>
-            <hr class="solid">
-            <v-card class="bg-green-lighten-4 my-3 d-flex justify-space-between">
-              <div class="d-flex">
-                <h1 class="text-green px-2">Income</h1>
-                <img class="pa-4 mx-1" src="/imageDash/moneyIncrease.png" height="100" />
-              </div>
-              
-              <h1 class="text-green ">0 Baht</h1>
-            </v-card>
-            <v-card class="bg-red-lighten-4 my-3 d-flex justify-space-between">
-              <div class="d-flex">
-                <h1 class="text-red px-2">Expend</h1>
-                <img class="pa-4 mx-1" src="/imageDash/moneyDecrease.png" height="100" />
-              </div>
-              
-              <h1 class="text-red ">0 Baht</h1>
-            </v-card>
-          </v-card>
-        </v-col>
-        
-
-        <!-- Income card -->
-        <v-col>
-          <v-card class="pa-2 px-5 rounded-xl ">
-            <h1>Income</h1>
-            <hr class="solid">
-            <v-card class="my-3 d-flex " flat>
-              <img class="ma-2 mx-3" src="/imageDash/hand-money.png" height="50" />
-              <div class="pa-2 mr-auto">
-                <h3>ฝากเงิน</h3>
-                <a>wallet</a>
-              </div>
-              <div class="align-center d-flex">
-                <div class="px-2">
-                  <a>5,000</a><br />
-                  <a>11/04/22</a>
-                </div>
-                <v-img src="../../../public/imageDash/greenReg.png" height="65" width="10" />
-              </div>
-            </v-card>
-            <!-- end of component income card -->
-            <!-- component income card -->
-            <v-card class="my-3 d-flex " flat>
-              <!-- picture  -->
-              <img class="ma-2 mx-3" src="/imageDash/hand-money.png" height="50" />
-
-              <div class="pa-2 mr-auto">
-                <h3>ฝากเงิน</h3>
-                <a>wallet</a>
-              </div>
-
-              <div class="align-center d-flex">
-                <div class="px-2">
-                  <a>5,000</a><br />
-                  <a>11/04/22</a>
-                </div>
-                <v-img src="../../../public/imageDash/greenReg.png" height="65" width="10" />
-              </div>
-            </v-card>
-            <!-- end of component income card -->
-            <!-- component income card -->
-            <v-card class="my-3 d-flex " flat>
-              <!-- picture  -->
-              <img class="ma-2 mx-3" src="/imageDash/bingo.png" height="50" />
-
-              <div class="pa-2 mr-auto">
-                <h3>หนูนาพาโชค</h3>
-                <a>wallet</a>
-              </div>
-
-              <div class="align-center d-flex">
-                <div class="px-2">
-                  <a>100,000</a><br />
-                  <a>11/04/22</a>
-                </div>
-                <v-img src="../../../public/imageDash/greenReg.png" height="65" width="10" />
-              </div>
-            </v-card>
-            <!-- end of component income card -->
-            <!-- component income card -->
-            <v-card class="my-3 d-flex " flat>
-              <!-- picture  -->
-              <img class="ma-2 mx-3" src="/imageDash/bingo.png" height="50" />
-
-              <div class="pa-2 mr-auto">
-                <h3>หนูนาพาโชค</h3>
-                <a>wallet</a>
-              </div>
-
-              <div class="align-center d-flex">
-                <div class="px-2">
-                  <a>100,000</a><br />
-                  <a>11/04/22</a>
-                </div>
-                <v-img src="../../../public/imageDash/greenReg.png" height="65" width="10" />
-              </div>
-            </v-card>
-            <!-- end of component income card -->
-
-          </v-card>
-
-          <!-- Expense -->
-          <v-card class="my-5 pa-2 px-5 rounded-xl">
-            <h1>Expense</h1>
-            <hr class="solid">
-
-            <!-- component income card -->
-            <v-card class="my-3 d-flex " flat>
-              <!-- picture  -->
-              <img class="ma-2 mx-3" src="/imageDash/hand-money.png" height="50" />
-
-              <div class="pa-2 mr-auto">
-                <h3>ถอนเงิน</h3>
-                <a>wallet</a>
-              </div>
-
-              <div class="align-center d-flex">
-                <div class="px-2">
-                  <a>5,000</a><br />
-                  <a>11/04/22</a>
-                </div>
-                <v-img src="../../../public/imageDash/redReg.png" height="65" width="10" />
-              </div>
-            </v-card>
-            <!-- end of component income card -->
-            <!-- component income card -->
-            <v-card class="my-3 d-flex " flat>
-              <!-- picture  -->
-              <img class="ma-2 mx-3" src="/imageDash/hand-money.png" height="50" />
-
-              <div class="pa-2 mr-auto">
-                <h3>ถอนเงิน</h3>
-                <a>wallet</a>
-              </div>
-
-              <div class="align-center d-flex">
-                <div class="px-2">
-                  <a>5,000</a><br />
-                  <a>11/04/22</a>
-                </div>
-                <v-img src="../../../public/imageDash/redReg.png" height="65" width="10" />
-              </div>
-            </v-card>
-            <!-- end of component income card -->
-            <!-- component income card -->
-            <v-card class="my-3 d-flex " flat>
-              <!-- picture  -->
-              <img class="ma-2 mx-3" src="/imageDash/bingo.png" height="50" />
-
-              <div class="pa-2 mr-auto">
-                <h3>หนูนาดับโชค</h3>
-                <a>wallet</a>
-              </div>
-
-              <div class="align-center d-flex">
-                <div class="px-2">
-                  <a>100,000</a><br />
-                  <a>11/04/22</a>
-                </div>
-                <v-img src="../../../public/imageDash/redReg.png" height="65" width="10" />
-              </div>
-            </v-card>
-            <!-- end of component income card -->
-            <!-- component income card -->
-            <v-card class="my-3 d-flex " flat>
-              <!-- picture  -->
-              <img class="ma-2 mx-3" src="/imageDash/bingo.png" height="50" />
-
-              <div class="pa-2 mr-auto">
-                <h3>หนูนาดับโชค</h3>
-                <a>wallet</a>
-              </div>
-
-              <div class="align-center d-flex">
-                <div class="px-2">
-                  <a>100,000</a><br />
-                  <a>11/04/22</a>
-                </div>
-                <v-img src="../../../public/imageDash/redReg.png" height="65" width="10" />
-              </div>
-            </v-card>
-            <!-- end of component income card -->
-
-          </v-card>
-        </v-col>
-        <!-- end of first col -->
+          <v-col>
+              <v-card>
+                  1
+              </v-card>
+          </v-col>
+          <v-col>
+              <v-card>
+                  2
+              </v-card>
+          </v-col>
       </v-row>
-      <!-- end of first row -->
-
-      <!-- start 2 row -->
-      <v-row>
-        <!-- start row 2 col 1 -->
-        <v-col>
-
-        </v-col>
-      </v-row>
-
-
-    </v-container>
-
-
-
   </v-container>
-
 </template>
+
+<style scoped>
+.gradient {
+  background: rgb(129,172,255);
+  background: linear-gradient(0deg, rgba(129,172,255,1) 0%, rgba(67,130,254,1) 100%);
+}
+</style>
