c7e3477856a3efab712a713c54f948e4362bb2df
Wasu <67475566+Wasuthoo@users.noreply.github.com>
2022-12-16 14:56:26 +0700

    update dashboard

--- src/views/app/dashboard.vue
+++ src/views/app/dashboard.vue
@@ -1,38 +1,57 @@
-
 <template>
   <v-container fluid class="bg-blue-lighten-5 pa-12" width="100%">
-      <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <hr class="solid">
+
+    <!-- Card -->
+    <v-card class="gradient d-flex justify-center pa-6 my-5 rounded-lg">
+      <img class="ma-1" src="/icon/user1.png" alt="user 1" height="120" width="120" />
+      <div class="ma-1 px-5">
+        <h1 class="text-center">สรุปรายการความเคลื่อนไหวบัญชีประจำปี</h1>
+        <h2 class="text-center text-white">คุณธนวรรณ ณ กรุงเทพมหานคร</h2>
+        <h2 class="text-center text-white">64050138@kmitl.ac.th</h2>
+      </div>
+    </v-card>
+
+
+    <v-row>
+      <v-col>
+        <v-card class="pa-6 bg-green-lighten-4 rounded-lg">
+          <h1 class="my-n2">ยอดเงินเข้ารวม</h1>
+          <h1 class="text-end">326,000 บาท</h1>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="pa-6 bg-red-lighten-4 rounded-lg">
+          <h1 class="my-n2">ยอดเงินออกรวม</h1>
+          <h1 class="text-end">326,000 บาท</h1>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!-- SummaryCard -->
+    <v-card class="my-6 pa-4 px-5 rounded-lg ">
+      <h1>สรุปรายการความเคลื่อนไหวบัญชีประจำเดือนธันวาคม</h1>
       <hr class="solid">
-
-      <!-- Card -->
-      <v-card class="gradient d-flex justify-center pa-6 my-5" >
-          <img class="ma-1" src="/icon/user1.png" alt="user 1" height="120" width="120" />
-          <div class="ma-1 px-5">
-              <h1 class="text-center">สรุปรายการความเคลื่อนไหวบัญชีประจำปี</h1>
-              <h2 class="text-center text-white">คุณธนวรรณ ณ กรุงเทพมหานคร</h2>
-              <h2 class="text-center text-white">64050138@kmitl.ac.th</h2>
-          </div>
-      </v-card>
-      <!-- end of card -->
-
-      <v-row>
-          <v-col>
-              <v-card>
-                  1
-              </v-card>
-          </v-col>
-          <v-col>
-              <v-card>
-                  2
-              </v-card>
-          </v-col>
-      </v-row>
+      <div class="d-flex pa-4">
+        <v-img src="/imageDash/Graph1.png" height="500" width="200" />
+        <div class="pa-3">
+          <v-card class="bg-green-lighten-5 pa-2 ma-3 rounded-lg" width="320">
+            <h3>income</h3>
+            <h1 class="text-end px-2">1,000 Baht</h1>
+          </v-card>
+          <v-card class="bg-red-lighten-5 pa-2 ma-3 rounded-lg" width="320">
+            <h3>expense</h3>
+            <h1 class="text-end px-2">3,000 Baht</h1>
+          </v-card>
+        </div>
+      </div>
+    </v-card>
   </v-container>
 </template>
 
 <style scoped>
 .gradient {
-  background: rgb(129,172,255);
-  background: linear-gradient(0deg, rgba(129,172,255,1) 0%, rgba(67,130,254,1) 100%);
+  background: rgb(129, 172, 255);
+  background: linear-gradient(0deg, rgba(129, 172, 255, 1) 0%, rgba(67, 130, 254, 1) 100%);
 }
 </style>
--- src/views/app/nearme.vue
+++ src/views/app/nearme.vue
@@ -1,404 +1,361 @@
 <template>
-    <div class = "bg">
-    <h1 class="pa-5">Food near me </h1>
-    <hr/>
-    
-    <v-row col = "6">
-        
-        <v-col>
-
-            <h2 class="pa-6" style="color: dodgerblue;">
-                Meetang    
-            </h2> 
-
-        </v-col>
-        
-        <v-col>
-
-        <v-form>
-
-            <v-container class="mx-auto">           
-
-                <v-text-field 
-                    solo
-                    label="กรุณาระบุตำแหน่งปัจจุบันของคุณ"
-                >
-
-                    <template v-slot:append>
-                        <v-icon color="red">mdi-map-marker</v-icon>
-                    </template>
-
-                </v-text-field>
-
-            </v-container>
-
-        </v-form> 
-     
-        </v-col>
-
-    </v-row>  
-
-  <div>
-
-    <!-- แถว 1 -->
-    <v-row>
-
-        <v-col>
-
-        <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-        
-        <v-img
-            class="white--text align-end"
-            height="200px"
-            src="/shop/Shop1BG.png"
-        >
-        </v-img>
-
-        <v-card-title>
-            มาดามตามสั่ง - วัดปากลมบน
-        </v-card-title>
-
-        <v-card-subtitle>
-            <br>
-        </v-card-subtitle>
-
-        <v-card-subtitle class="my-2">
-            <v-icon color="orange">
-                mdi-star
-            </v-icon>
-            4.9
-            <v-icon color="black">
-                mdi-walk
-            </v-icon>
-            0.3 km
-        </v-card-subtitle>
-
-        </v-card>
-        
-        </v-col>
-
-        <v-col>
-            <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-        
-        <v-img
-            class="white--text align-end"
-            height="200px"
-            src="/shop/Shop2BG.png"
-        >
-        </v-img>
-
-        <v-card-title>
-            ข้าวมันไก่ป้าน้อย
-        </v-card-title>
-
-        <v-card-subtitle>
-            <br>
-        </v-card-subtitle>
-
-        <v-card-subtitle class="my-2">
-            <v-icon color="orange">
-                mdi-star
-            </v-icon>
-            4.4
-            <v-icon color="black">
-                mdi-walk
-            </v-icon>
-            0.5 km
-        </v-card-subtitle>
-
-        </v-card>
-
-        </v-col>
-
-        <v-col>
-            
-        <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-    
-        <v-img
-            class="white--text align-end"
-            height="200px"
-            src="/shop/Shop3BG.png"
-        >
-        </v-img>
-
-        <v-card-title>
-            ก๊วยเตี๋ยวต้มยำเฮียเล้ง
-        </v-card-title>
-
-        <v-card-subtitle>
-            <br>
-        </v-card-subtitle>
-
-        <v-card-subtitle class="my-2">
-            <v-icon color="orange">
-                mdi-star
-            </v-icon>
-            4.6
-            <v-icon color="black">
-                mdi-walk
-            </v-icon>
-            0.8 km
-        </v-card-subtitle>
-
-        </v-card>
-
-        </v-col>
-
-    </v-row>
-
-    <!-- แถว 2 -->
-    <v-row>
-
-    <v-col>
-
-    <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-
-    <v-img
-        class="white--text align-end"
-        height="200px"
-        src="/shop/Shop4BG.png"
-    >
-    </v-img>
-
-    <v-card-title>
-        หมูปิ้งลุงรัง
-    </v-card-title>
-
-    <v-card-subtitle>
-        <br>
-    </v-card-subtitle>
-
-    <v-card-subtitle class="my-2">
-        <v-icon color="orange">
-            mdi-star
-        </v-icon>
-        3.9
-        <v-icon color="black">
-            mdi-walk
-        </v-icon>
-        1.0 km
-    </v-card-subtitle>
-
-    </v-card>
-
-    </v-col>
-
-    <v-col>
-        <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-
-    <v-img
-        class="white--text align-end"
-        height="200px"
-        src="/shop/Shop5BG.png"
-    >
-    </v-img>
-
-    <v-card-title>
-        ส้มตำสังค์ทอง
-    </v-card-title>
-
-    <v-card-subtitle>
-        <br>
-    </v-card-subtitle>
-
-    <v-card-subtitle class="my-2">
-        <v-icon color="orange">
-            mdi-star
-        </v-icon>
-        3.4
-        <v-icon color="black">
-            mdi-walk
-        </v-icon>
-        1.3 km
-    </v-card-subtitle>
-
-    </v-card>
-
-    </v-col>
-
-    <v-col>
-        
-    <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-
-    <v-img
-        class="white--text align-end"
-        height="200px"
-        src="/shop/Shop6BG.png"
-    >
-    </v-img>
-
-    <v-card-title>
-        สเต็กพี่โตเนื้อแน่น
-    </v-card-title>
-
-    <v-card-subtitle>
-        <br>
-    </v-card-subtitle>
-
-    <v-card-subtitle class="my-2">
-        <v-icon color="orange">
-            mdi-star
-        </v-icon>
-        5.0
-        <v-icon color="black">
-            mdi-walk
-        </v-icon>
-        1.4 km
-    </v-card-subtitle>
-
-    </v-card>
-
-    </v-col>
-
-    </v-row>
-
-<!-- แถวที่ 3 -->
-    <v-row>
-
-<v-col>
-
-<v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-
-<v-img
-    class="white--text align-end"
-    height="200px"
-    src="/shop/Shop7BG.png"
->
-</v-img>
-
-<v-card-title>
-    ข้าวแกงกะหรี่พรี่คาซึยะ
-</v-card-title>
-
-<v-card-subtitle>
-    <br>
-</v-card-subtitle>
-
-<v-card-subtitle class="my-2">
-    <v-icon color="orange">
-        mdi-star
-    </v-icon>
-    4.7
-    <v-icon color="black">
-        mdi-walk
-    </v-icon>
-    1.5 km
-</v-card-subtitle>
-
-</v-card>
-
-</v-col>
-
-<v-col>
-    <v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-
-<v-img
-    class="white--text align-end"
-    height="200px"
-    src="/shop/Shop8BG.png"
->
-</v-img>
-
-<v-card-title>
-    ซูชิไข่เค็ม
-</v-card-title>
-
-<v-card-subtitle>
-    <br>
-</v-card-subtitle>
-
-<v-card-subtitle class="my-2">
-    <v-icon color="orange">
-        mdi-star
-    </v-icon>
-    4.2
-    <v-icon color="black">
-        mdi-walk
-    </v-icon>
-    1.5 km
-</v-card-subtitle>
-
-</v-card>
-
-</v-col>
-
-<v-col>
-    
-<v-card @click="handleCardClick" class="mx-5" max-width="361" to = 'detailshop'>
-
-<v-img
-    class="white--text align-end"
-    height="200px"
-    src="/shop/Shop9BG.png"
->
-</v-img>
-
-<v-card-title>
-    ชาชักกระรอกบิน
-</v-card-title>
-
-<v-card-subtitle>
-    <br>
-</v-card-subtitle>
-
-<v-card-subtitle class="my-2">
-    <v-icon color="orange">
-        mdi-star
-    </v-icon>
-    4.8
-    <v-icon color="black">
-        mdi-walk
-    </v-icon>
-    1.7 km
-</v-card-subtitle>
-
-</v-card>
-
-</v-col>
-
-</v-row>
-
-    </div>
-    <!-- จบหน้าร้าน -->
-
-<div class="justify-center align-center">
-
-    <v-container class="justify-center align-center">
-        
-<v-btn class="justify-center align-center"
-    size="x-large"
-    variant="outlined"
-    color="blue"
->
-ดูเพิ่มเติม
-</v-btn>
-
-</v-container>
-
-</div>
-</div>
+	<div class="bg">
+		<h1 class="pa-5">Food near me </h1>
+		<hr />
+
+		<v-row col="6">
+
+			<v-col>
+
+				<h2 class="pa-6" style="color: dodgerblue;">
+					Meetang
+				</h2>
+
+			</v-col>
+
+			<v-col>
+
+				<v-form>
+
+					<v-container class="mx-auto">
+
+						<v-text-field solo label="กรุณาระบุตำแหน่งปัจจุบันของคุณ">
+
+							<template v-slot:append>
+								<v-icon color="red">mdi-map-marker</v-icon>
+							</template>
+
+						</v-text-field>
+
+					</v-container>
+
+				</v-form>
+
+			</v-col>
+
+		</v-row>
+
+		<div>
+
+			<!-- แถว 1 -->
+			<v-row>
+
+				<v-col>
+
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop1BG.png">
+						</v-img>
+
+						<v-card-title>
+							มาดามตามสั่ง - วัดปากลมบน
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							4.9
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							0.3 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+				<v-col>
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop2BG.png">
+						</v-img>
+
+						<v-card-title>
+							ข้าวมันไก่ป้าน้อย
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							4.4
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							0.5 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+				<v-col>
+
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop3BG.png">
+						</v-img>
+
+						<v-card-title>
+							ก๊วยเตี๋ยวต้มยำเฮียเล้ง
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							4.6
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							0.8 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+			</v-row>
+
+			<!-- แถว 2 -->
+			<v-row>
+
+				<v-col>
+
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop4BG.png">
+						</v-img>
+
+						<v-card-title>
+							หมูปิ้งลุงรัง
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							3.9
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							1.0 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+				<v-col>
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop5BG.png">
+						</v-img>
+
+						<v-card-title>
+							ส้มตำสังค์ทอง
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							3.4
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							1.3 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+				<v-col>
+
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop6BG.png">
+						</v-img>
+
+						<v-card-title>
+							สเต็กพี่โตเนื้อแน่น
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							5.0
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							1.4 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+			</v-row>
+
+			<!-- แถวที่ 3 -->
+			<v-row>
+
+				<v-col>
+
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop7BG.png">
+						</v-img>
+
+						<v-card-title>
+							ข้าวแกงกะหรี่พรี่คาซึยะ
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							4.7
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							1.5 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+				<v-col>
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop8BG.png">
+						</v-img>
+
+						<v-card-title>
+							ซูชิไข่เค็ม
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							4.2
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							1.5 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+				<v-col>
+
+					<v-card @click="handleCardClick" class="mx-5" max-width="361" to='detailshop'>
+
+						<v-img class="white--text align-end" height="200px" src="/shop/Shop9BG.png">
+						</v-img>
+
+						<v-card-title>
+							ชาชักกระรอกบิน
+						</v-card-title>
+
+						<v-card-subtitle>
+							<br>
+						</v-card-subtitle>
+
+						<v-card-subtitle class="my-2">
+							<v-icon color="orange">
+								mdi-star
+							</v-icon>
+							4.8
+							<v-icon color="black">
+								mdi-walk
+							</v-icon>
+							1.7 km
+						</v-card-subtitle>
+
+					</v-card>
+
+				</v-col>
+
+			</v-row>
+
+		</div>
+		<!-- จบหน้าร้าน -->
+
+		<div class="justify-center align-center">
+
+			<v-container class="justify-center align-center">
+
+				<v-btn class="justify-center align-center" size="x-large" variant="outlined" color="blue">
+					ดูเพิ่มเติม
+				</v-btn>
+
+			</v-container>
+
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  methods: {
-    handleCardClick() {
-      // add logic to handle card click here
-    }
-  }
+	methods: {
+		handleCardClick() {
+			// add logic to handle card click here
+		}
+	}
 }
 </script>
 
 <style>
-    .bg{
-        background-color: #EEF7FF;
-    }
+.bg {
+	background-color: #EEF7FF;
+}
 </style>
 
 <style>
-    .ct{
-        align-items: center;
-    }
+.ct {
+	align-items: center;
+}
 </style>
 
 
