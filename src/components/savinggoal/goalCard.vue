cc31813d21896bf3ace69fad88bffc384a71b33f
NongNon <attnon@gmail.com>
2022-12-19 06:48:04 +0700

    push but to Project

--- src/components/savinggoal/goalCard.vue
+++ src/components/savinggoal/goalCard.vue
@@ -1,77 +1,98 @@
 <script>
-    export default {
-        name: "goalCard",
-        props: {
-            savingGoal: {
-                type: Array,
-                required: true,
-            },
+import Datepicker from 'vue3-datepicker';
+export default {
+    name: "goalCard",
+    props: {
+        savingGoal: {
+            type: Array,
+            required: true,
         },
-        methods: {
-            onClickCard(goal) {
-                this.selectedGoal = {...goal}
-                this.showdialog = true;
-            },
-            onSaveGoal() {
-                let index = this.savingGoal.findIndex(goal => goal.id === this.selectedGoal.id);
-                this.savingGoal[index] = {...this.selectedGoal};
-                this.editMode = false;
-                this.showdialog = false;
-            },
-            onCancelGoal() {
-                this.editMode = false;
-                this.showdialog = false;
-            },
-            onDeleteGoal() {
-                let index = this.savingGoal.findIndex(goal => goal.id === this.selectedGoal.id);
-                this.savingGoal.splice(index, 1);
-                this.editMode = false;
-                this.showdialog = false;
-            },
+    },
+    components: {
+        Datepicker
+    },
+    methods: {
+        onClickCard(goal) {
+            this.selectedGoal = { ...goal }
+            this.showdialog = true;
         },
-        data() {
-            return {
-                showdialog: false,
-                editMode: false,
-                selectedGoal: {},
-            };
+        onSaveGoal() {
+            let index = this.savingGoal.findIndex(goal => goal.id === this.selectedGoal.id);
+            this.savingGoal[index] = { ...this.selectedGoal };
+            this.editMode = false;
+            this.showdialog = false;
         },
-    };
+        onCancelGoal() {
+            this.editMode = false;
+            this.showdialog = false;
+        },
+        onDeleteGoal() {
+            let index = this.savingGoal.findIndex(goal => goal.id === this.selectedGoal.id);
+            this.savingGoal.splice(index, 1);
+            this.editMode = false;
+            this.showdialog = false;
+        },
+    },
+    data() {
+        return {
+            showdialog: false,
+            editMode: false,
+            selectedGoal: {},
+            form: {
+                name: "",
+                amount: "",
+                date: null,
+                description: "",
+            },
+        };
+    },
+};
 </script>
 
 <template>
-    <v-card class="ma-5 bg-blue-lighten-4" @click="onClickCard(goal)" v-for="goal in savingGoal" :key="goal.id">
-        <v-card-title >{{ goal.name }}</v-card-title>
-        <v-card-text class="mx-2">{{ goal.amount }}</v-card-text>
-        <v-card-subtitle class="mx-2">{{ goal.date }}</v-card-subtitle>
+    <v-card style="margin:20px 100px; border:1px solid black; " outlined max-width="1200px" @click="onClickCard(goal)"
+        v-for="goal in savingGoal" :key="goal.id">
+        <v-card-title>{{ goal.name }}</v-card-title>
+        <v-card-text class="mx-2"> {{ goal.saving }} / {{ goal.amount }}</v-card-text>
+        <v-card-subtitle class="mx-2">{{ Math.floor((new Date(goal.date) - new Date()) / (1000 * 3600 * 24)) }} days left.
+            End in {{ new Date(goal.date).toLocaleString('en-US', { year: 'numeric', month: 'short', day: 'numeric' }) }}
+        </v-card-subtitle>
         <v-card-text class="mx-2">{{ goal.description }}</v-card-text>
     </v-card>
+
 
     <v-dialog v-model="showdialog" max-width="500px">
         <v-card>
             <v-card-title>
-                <span class="headline pa-6"> {{ selectedGoal.name }} </span>
+                <span class="headline ma-6">Add a new saving goal</span>
             </v-card-title>
 
             <v-card-text>
                 <v-container>
                     <v-row>
                         <v-col cols="12">
-                            <v-text-field v-model="selectedGoal.name" id="name" label="Name" :readonly="!editMode"></v-text-field>
+                            <v-text-field id="name" label="Name" required variant="outlined"
+                                v-model="form.name"></v-text-field>
+                        </v-col>
+
+                        <v-col cols="12">
+                            <v-text-field id="amount" label="Amount" required variant="outlined"
+                                v-model="form.amount"></v-text-field>
+                        </v-col>
+
+                        <v-col cols="12">
+                            <label for="date" class="label">Date end</label>
+                            <Datepicker v-model="form.date" class="text-field" label="Date" variant="outlined"
+                                placeholder="YYYY-MM-DD" />
                         </v-col>
                         <v-col cols="12">
-                            <v-text-field v-model="selectedGoal.amount" id="amount" label="Amount" :readonly="!editMode"></v-text-field>
-                        </v-col>
-                        <v-col cols="12">
-                            <v-text-field v-model="selectedGoal.date" id="date" label="Date" :readonly="!editMode"></v-text-field>
-                        </v-col>
-                        <v-col cols="12">
-                            <v-text-field v-model="selectedGoal.description" id="description" label="Description" :readonly="!editMode"></v-text-field>
+                            <v-textarea clearable label="discription" variant="outlined"
+                                v-model="form.description"></v-textarea>
                         </v-col>
                     </v-row>
                 </v-container>
             </v-card-text>
-            
+
             <v-card-actions v-if="editMode">
                 <v-spacer></v-spacer>
                 <v-btn color="red darken-1" text @click="onDeleteGoal">DELETE</v-btn>
@@ -81,12 +102,12 @@
 
             <v-card-actions v-else>
                 <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="editMode=true">EDIT</v-btn>
+                <v-btn color="blue darken-1" text @click="editMode = true">EDIT</v-btn>
                 <v-btn color="blue darken-1" text @click="showdialog = false">CLOSE</v-btn>
             </v-card-actions>
         </v-card>
     </v-dialog>
 
-    
+
 </template>
 
--- src/views/app/savingGoal.vue
+++ src/views/app/savingGoal.vue
@@ -1,3 +1,67 @@
+<template>
+    <h1 class="pa-5">Saving Goal</h1>
+    <hr />
+    <v-divider></v-divider>
+
+    <!-- <v-card @click="onClickCard" v-for="goal in savingGoal" :key="goal.id">
+        <v-card-title>{{ goal.name }}</v-card-title>
+        <v-card-subtitle>{{ goal.amount }}</v-card-subtitle>
+        <v-card-text>{{ goal.description }}</v-card-text>
+    </v-card> -->
+
+    <goalCard v-if="this.acc.goals.length > 0" :savingGoal="this.acc.goals"></goalCard>
+    <div v-else class="bg-purple-darken-2"
+        style="display:absolute; margin:auto; top:0px; bottom:0px; left:0px; right:0px;">
+        <h1 class="pa-5 text-medium-emphasis ">No aGoals</h1>
+    </div>
+
+
+    <!-- add a circle button to bottom right-->
+    <v-btn @click="addForm = !addForm" style="position: fixed; bottom: 5vh; right: 5vw;" icon="mdi-plus" color="primary"
+        size="large">
+    </v-btn>
+
+    <v-dialog v-model="addForm" max-width="500px">
+        <v-card>
+            <v-card-title>
+                <span class="headline ma-6">Add a new saving goal</span>
+            </v-card-title>
+
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field id="name" label="Name" required variant="outlined"
+                                v-model="form.name"></v-text-field>
+                        </v-col>
+
+                        <v-col cols="12">
+                            <v-text-field id="amount" label="Amount" required variant="outlined"
+                                v-model="form.amount"></v-text-field>
+                        </v-col>
+
+                        <v-col cols="12">
+                            <label for="date" class="label">Date end</label>
+                            <Datepicker v-model="form.date" class="text-field" label="Date" variant="outlined"
+                                placeholder="YYYY-MM-DD" />
+                        </v-col>
+                        <v-col cols="12">
+                            <v-textarea clearable label="discription" variant="outlined"
+                                v-model="form.description"></v-textarea>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="addForm = false">Cancel</v-btn>
+                <v-btn color="blue darken-1" text @click="onSaveGoal">Save</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
 
 <script>
 import goalCard from "..//..//components//savinggoal//goalCard.vue";
@@ -64,13 +128,14 @@ export default {
         onSaveGoal() {
             console.log(this.form);
             let newGoal = {
-                id: this.Goals.length + 1,
+                id: new Date().getTime(),
                 name: this.form.name,
+                saving: 0,
                 amount: this.form.amount,
-                date: this.form.date,
+                date: this.form.date.toJSON(),
                 description: this.form.description,
             };
-            this.Goals.push(newGoal);
+            this.acc.goals.push(newGoal);
             this.addForm = false;
         }
 
@@ -82,70 +147,6 @@ export default {
     },
 };
 </script>
-
-<template>
-    <h1 class="pa-5">Saving Goal</h1>
-    <hr />
-    <v-divider></v-divider>
-
-    <!-- <v-card @click="onClickCard" v-for="goal in savingGoal" :key="goal.id">
-        <v-card-title>{{ goal.name }}</v-card-title>
-        <v-card-subtitle>{{ goal.amount }}</v-card-subtitle>
-        <v-card-text>{{ goal.description }}</v-card-text>
-    </v-card> -->
-
-    <goalCard v-if="this.Goals.length > 0" :savingGoal="this.Goals"></goalCard>
-    <div v-else class="bg-purple-darken-2"
-        style="display:absolute; margin:auto; top:0px; bottom:0px; left:0px; right:0px;">
-        <h1 class="pa-5 text-medium-emphasis ">No aGoals</h1>
-    </div>
-
-
-    <!-- add a circle button to bottom right-->
-    <v-btn @click="addForm = !addForm" style="position: fixed; bottom: 5vh; right: 5vw;" icon="mdi-plus" color="primary"
-        size="large">
-    </v-btn>
-
-    <v-dialog v-model="addForm" max-width="500px">
-        <v-card>
-            <v-card-title>
-                <span class="headline ma-6">Add a new saving goal</span>
-            </v-card-title>
-
-            <v-card-text>
-                <v-container>
-                    <v-row>
-                        <v-col cols="12">
-                            <v-text-field id="name" label="Name" required variant="outlined"
-                                v-model="form.name"></v-text-field>
-                        </v-col>
-
-                        <v-col cols="12">
-                            <v-text-field id="amount" label="Amount" required variant="outlined"
-                                v-model="form.amount"></v-text-field>
-                        </v-col>
-
-                        <v-col cols="12">
-                            <label for="date" class="label">Date end</label>
-                            <Datepicker v-model="form.date" class="text-field" label="Date" variant="outlined"
-                                placeholder="YYYY-MM-DD" />
-                        </v-col>
-                        <v-col cols="12">
-                            <v-textarea clearable label="discription" variant="outlined"
-                                v-model="form.description"></v-textarea>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="addForm = false">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="onSaveGoal">Save</v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-dialog>
-</template>
 
 <style>
 .text-field {
